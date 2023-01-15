a08202250cfa3c96f2dad7b251b1f6da879e8538
anashkagonit <anashkagonit@gmail.com>
2023-01-15 21:30:41 +0300

    Зазработка cart in header

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,7 +1,21 @@
 import { FC } from 'react'
 
+import styles from './Cart.module.scss'
+import CartItem from './cart-item/CartItem'
+
 const Cart: FC = () => {
-	return <div>Cart</div>
+	return (
+		<div className={styles['wrapper-cart']}>
+			<div className={styles.heading}>
+				<span className={styles.badge}>1</span>
+				<span className={styles.tetx}>MY BASKET </span>
+			</div>
+
+			<div className={styles.cart}>
+				<CartItem />
+			</div>
+		</div>
+	)
 }
 
 export default Cart
---
+++ app/data/product.data.ts
@@ -0,0 +1,12 @@
+import { IProduct } from "@/types/product.interface";
+
+export const products: IProduct[] = [
+  {
+    id:1,
+    description: 'Description By product number 1',
+    name: 'Midnight Mint Mocha Frappuchino',
+    images: [],
+    price: 9,
+    reviews: []
+  }
+]
