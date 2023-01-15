7c70cd98d8ecaabd9a5e87841e9ad5b95f5498cc
anashkagonit <anashkagonit@gmail.com>
2023-01-15 15:46:28 +0300

    end 1 lesson

--- app/components/layout/header/search/Search.tsx
+++ app/components/layout/header/search/Search.tsx
@@ -1,11 +1,28 @@
-import { FC } from 'react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { FC, useState } from 'react'
 
 import styles from './Search.module.scss'
 
 const Search: FC = () => {
+	const [searchTerm, setSearchTerm] = useState(' ')
+	console.log(searchTerm, setSearchTerm)
+
 	return (
 		<div className={styles.search}>
-			<div>Search</div>
+			<InputGroup>
+				<InputLeftElement
+					pointerEvents='none'
+					children={<SearchIcon color='gray.300' />}
+				/>
+				<Input
+					variant='outline'
+					type='search'
+					onChange={e => setSearchTerm(e.target.value)}
+					value={searchTerm}
+					placeholder='Search'
+				/>
+			</InputGroup>
 		</div>
 	)
 }
