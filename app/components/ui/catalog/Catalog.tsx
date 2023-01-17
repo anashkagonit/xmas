843447b21d030ba862f53b01551b146f44ae4f76
anashkagonit <anashkagonit@gmail.com>
2023-01-17 20:59:39 +0300

    start create carousel

---
+++ app/components/ui/catalog/Catalog.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import Carousel from './carousel/Carousel'
+
+const Catalog: FC = () => {
+	return (
+		<div>
+			<Carousel />
+		</div>
+	)
+}
+
+export default Catalog
